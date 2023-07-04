3b98c68bbfe7ad78040afa7c49fc5d01f5c7ebf8
Zahid Abdullah <zaevanphy@gmail.com>
2023-07-04 12:49:10 +0300

    Lession 1

---
+++ build/js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const username = "LetMeComeIn";
+const password = "Letmein1234!";
+console.log(username, password);
+//var is for old browsers, let is for new browsers
+const a = 12;
+const b = "2"; //fix const b: number = "2";
+const c = 3;
+console.log(a / b + c);
+//noEmitonError: true when its enabled, if there is error , it will not create the js file
+//turnthis on when if you DO NOT WANT TO CREATE JS FILE IF THERE IS ERROR
+console.log(c * b);
+//End of lesson 1
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+const username = "LetMeComeIn";  
+const password = "Letmein1234!";
+console.log(username, password);
+
+//var is for old browsers, let is for new browsers
+
+const a: number = 12;
+const b: string = "2"; //fix const b: number = "2";
+const c: number = 3;
+console.log(a/b + c);
+
+//noEmitonError: true when its enabled, if there is error , it will not create the js file
+//turnthis on when if you DO NOT WANT TO CREATE JS FILE IF THERE IS ERROR
+
+console.log(c*b);
+
+//End of lesson 1
